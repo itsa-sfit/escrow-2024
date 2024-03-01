e1c7ce87d25e5901d66b2b2dcf5df6b5c7bc8cd2
Ajaykumar <ajaykumarn3000@gmail.com>
2024-03-01 19:37:46 +0530

    Success update

--- frontend/src/components/Success.js
+++ frontend/src/components/Success.js
@@ -1,28 +1,34 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import useUserContext from "../hooks/useUserContext";
 import { serverUrl } from "../setup";
 
-const getTime = async (user) => {
-  try {
-    const response = await fetch(serverUrl + "/time/sendtime", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${user.token}`,
-    },
-  });
-  const json = await response.json();
-  return json.total_time;
-  } catch (e) {
-    console.log(e);
-  }
-  
-}
 const Success = () => {
-  const {user} = useUserContext();
+  const { user } = useUserContext();
+  const [time, setTime] = useState(null);
+
+  useEffect(() => {
+    const fetchTime = async () => {
+      try {
+        const response = await fetch(serverUrl + "/time/sendtime", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${user.token}`,
+          },
+        });
+        const json = await response.json();
+        setTime(json.total_time);
+      } catch (error) {
+        console.error("Error fetching time:", error);
+      }
+    };
+
+    fetchTime();
+  }, [user.token]);
+
   return (
     <h1 className="text-white text-2xl font-bold">
-      Congratulations! You have completed the quiz in {time}
+      Congratulations! You have completed the quiz in {time}.
     </h1>
   );
 };
