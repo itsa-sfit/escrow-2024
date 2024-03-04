e727b84597e253a696a6a301cb48a308801d2f63
Ajaykumar <ajaykumarn3000@gmail.com>
2024-03-04 10:30:44 +0530

    ReStart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,23 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
-// import Login from "./components/Login";
-// import Home from "./components/Home";
+import Login from "./components/Login";
+import Home from "./components/Home";
 import BG from "./components/BG";
-// import useUserContext from "./hooks/useUserContext";
-import TheEnd from "./components/TheEnd";
+import useUserContext from "./hooks/useUserContext";
+// import TheEnd from "./components/TheEnd";
 
 function App() {
-  // const { user } = useUserContext();
+  const { user } = useUserContext();
   return (
     <>
       <div className="App h-dvh desktop-hidden p-4 min-h-dvh w-full">
         <BG />
         <BrowserRouter>
           <Routes>
-            <Route path="/" element=<TheEnd /> />
-            {/* <Route path="/ajay" element={}/> */}
-            {/* <Route
-              path="/login"
-              element={user ? <Navigate to="/" /> : <Login />}
-            /> */}
+            <Route
+              path="/"
+              element={user ? <Home /> :<Login/>}
+            />
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </BrowserRouter>
